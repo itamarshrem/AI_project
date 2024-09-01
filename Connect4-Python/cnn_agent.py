7ded2bed7614cfe55dd3530eba1dc61244d8c1b6
Itamar Shrem <itamar.shrem@hpe.com>
2024-09-01 22:07:28 +0300

    [main] don't trust cnn_agent.py
    Branch: [main]

--- Connect4-Python/cnn_agent.py
+++ Connect4-Python/cnn_agent.py
@@ -1,9 +1,16 @@
 import numpy as np
 import torch
 from torch import nn
-from board import Board
+# from board import Board
 from torch.utils.data import DataLoader, TensorDataset
-import utils
+# import utils
+import pickle
+
+EMPTY_CELL = -1
+
+def get_qtable_from_file(full_path_filename):
+    with open(full_path_filename, 'rb') as file_object:
+        return pickle.load(file_object)
 
 class CnnAgent:
     # by getting a board the agent will run a cnn model to get the value of the model and than return the best action
@@ -72,7 +79,7 @@ class CnnAgent:
         # the qtable dataset contains 732597 keys
         #the board_grades contains 697848 entries
         # create a data set for the model from the q_table
-        q_table = utils.get_qtable_from_file(path_to_q_table)
+        q_table = get_qtable_from_file(path_to_q_table)
         board_grades = {}
         for state, action_values in q_table.items():
             board = np.array(state).reshape(*self.board_shape)
@@ -105,7 +112,7 @@ class CnnAgent:
 
     def place_disc(self, board, col, player_index):
         board = board.copy()
-        row = np.argmax(board[:, col] == Board.EMPTY_CELL)
+        row = np.argmax(board[:, col] == EMPTY_CELL)
         board[row, col] = player_index
         return board
 
@@ -113,24 +120,24 @@ class CnnAgent:
 
 LR = 0.01
 BATCH_SIZE = 20
-EPOCHS = 125
+EPOCHS = 1
 device = ("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class CNN(nn.Module):
-    def __init__(self, board_shape, winning_streak, num_of_players):
+    def __init__(self, board_shape, winning_streak):
         super(CNN, self).__init__()
+        self.num_of_players, self.rows, self.cols = board_shape
         self.winning_streak = winning_streak
-        self.conv1 = nn.Conv2d(num_of_players, 12, winning_streak, stride=1)  # 6 X 7 -> 3 X 4
+        self.conv1 = nn.Conv2d(self.num_of_players, 12, winning_streak, stride=1)  # 6 X 7 -> 3 X 4
         self.conv2 = nn.Conv2d(12, 12, 3, stride=1)  # 3 X 4 -> 1 X 2
-        self.fc1 = nn.Linear((board_shape[0] - 2 * winning_streak) * (board_shape[1] - 2 * winning_streak) * 12, 50)
+        self.fc1 = nn.Linear((self.rows - 2 * winning_streak) * (self.cols - 2 * winning_streak) * 12, 50)
         self.fc2 = nn.Linear(50, 1)
-        self.board_shape = board_shape
 
     def forward(self, x):
         x = torch.relu(self.conv1(x))
         x = torch.relu(self.conv2(x))
-        x = x.view(-1, (self.board_shape[0] - 2 * self.winning_streak) * (self.board_shape[1] - 2 * self.winning_streak) * 12)
+        x = x.view(-1, (self.rows - 2 * self.winning_streak) * (self.cols - 2 * self.winning_streak) * 12)
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -144,37 +151,37 @@ def parse_args():
     default_players_num = 2
     parser.add_argument('-size', '--board_shape', type=int, nargs=3, default=[6, 7, 1], help='size of the board')
     parser.add_argument('-d', '--depths', type=int, nargs="*", default=[2] * default_players_num, help='Depth of the search tree')
+    parser.add_argument('-ws', '--winning_streak', type=int, default=4, help='Number of consecutive pieces to win')
     parser.add_argument('-ng', '--num_of_games', type=int, default=1, help='Number of consecutive games')
 
     return parser.parse_args()
 def main():
-    model = None
     args = parse_args()
-    cnn_agent = CnnAgent(model, 0, 4, args.board_shape)
-    full_path_filename = utils.get_rl_agent_save_path(4, args.board_shape, 0, 2)
-    dataset = cnn_agent.create_data_set(full_path_filename, 2)
+    # full_path_filename = utils.get_rl_agent_save_path(4, args.board_shape, 0, 2)
+    # dataset = cnn_agent.create_data_set(full_path_filename, 2)
+    #
+    # # save dataset to Connect4-Python/cnn/cnn_dataset.pt
+    # torch.save(dataset, 'cnn/cnn_dataset.pt')
 
-    # save dataset to Connect4-Python/cnn/cnn_dataset.pt
-    torch.save(dataset, 'cnn/cnn_dataset.pt')
-
-    # dataset = torch.load('cnn/cnn_dataset.pt')
-    train, test = torch.utils.data.random_split(dataset, [int(0.8 * len(dataset)), len(dataset) - int(0.8 * len(dataset)])
+    dataset = torch.load('cnn/cnn_dataset.pt')
+    train, test = torch.utils.data.random_split(dataset, [int(0.8 * len(dataset)), len(dataset) - int(0.8 * len(dataset))])
     train_loader = DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
     test_loader = DataLoader(test, batch_size=BATCH_SIZE, shuffle=True)
-
+    num_of_players = 2
     # run the model on mac with cpu and on colab with gpu
-    model = CNN(args.board_shape, 4, 2).to(device)
+    cnn_agent = CnnAgent(args.board_shape, 0, args.winning_streak, num_of_players)
+    cnn_model = CNN(args.board_shape, args.winning_streak, num_of_players)
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+    optimizer = torch.optim.Adam(cnn_model.parameters(), lr=LR)
 
     loss_arr_train = []
     loss_arr_test = []
     for epoch in range(EPOCHS):
         print(f'Epoch {epoch + 1}\n-------------------------------')
-        cnn_agent.train_model(train_loader, model, loss_fn, optimizer, loss_arr_train)
-        cnn_agent.test_model(test_loader, model, loss_fn, loss_arr_test)
+        cnn_agent.train_model(train_loader, cnn_model, loss_fn, optimizer, loss_arr_train)
+        cnn_agent.test_model(test_loader, cnn_model, loss_fn, loss_arr_test)
+        torch.save(cnn_agent, 'cnn/cnn_model_weights.pt')
     print('Done!')
-    torch.save(model, 'cnn/cnn_model.pt')
     print('Model saved!')
 
 if __name__ == '__main__':
