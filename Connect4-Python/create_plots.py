c85a5f248c72269b061cda87f910c544e624988b
itamarshrem <itamar5250@gmail.com>
2024-09-07 19:46:00 +0300

    updated the report, added charts, added code to create plots

---
+++ Connect4-Python/create_plots.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#a function that gets several lists of data and plots them
+def plot_data(data, labels, title, x_label, y_label):
+    #add x axis array, with specific values. for example i want the x values to be 0.6, 0.4, 0.2, 0, and the y values will be the corresponding values in the data lists
+    x = np.arange(1, len(data[0]) + 1)
+    # plt.xticks(x, [0.6, 0.4, 0.2, 0]) # to run with gammas
+    #plot each list of data with the corresponding label
+
+    for i in range(len(data)):
+        #if labels[i] contains the word ibef2, plot it with strips
+        if "ibef2" in labels[i]:
+            plt.plot(x, data[i], label=labels[i], linestyle='dashed')
+        else:
+            plt.plot(x,data[i], label=labels[i])
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    if len(labels) > 1:
+        plt.legend()
+    #and save the plot to a file
+    plt.savefig(title + ".png")
+    plt.show()
+
+
+#graph for gammmas
+def graph_for_gammmas():
+    vs_offensive = [71, 81, 96, 100]
+    vs_defensive = [96, 99, 100, 100]
+    labels = ["offensive", "defensive"]
+    data = [vs_offensive, vs_defensive]
+    plot_data(data, labels, "performance against baseline players", "probability to random action",
+              "percentage of wins(on 100 games)")
+
+def graph_for_depths():
+    complex_vs_offensive = [95.1, 95.2, 99.0, 99.9, 99.9, 99.9]
+    complex_vs_defensive = [86.3, 95.5, 93.1, 98.1] # todo need aboud results!
+    ibef2_vs_offensive = [83.1, 76.5, 97.5, 81.8, 98.5, 85.2]
+    ibef2_vs_defensive = [89.9, 85.7, 98.2, 92.3, ] # todo need aboud results!
+    labels = ["offensive vs complex", "defensive vs complex", "offensive vs ibef2", "defensive vs ibef2"]
+    data = [complex_vs_offensive, complex_vs_defensive, ibef2_vs_offensive, ibef2_vs_defensive]
+    plot_data(data, labels, "Compare complex and ibef2 heuristics", "depth of the search tree",
+              "percentage of wins(on 1000 games)")
+
+if __name__ == '__main__':
+    graph_for_gammmas()
+
