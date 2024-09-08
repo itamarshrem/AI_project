0c616dfe97e6ae875a3dd50787606119c69ac48e
itamarshrem <itamar5250@gmail.com>
2024-09-08 17:36:15 +0300

    updated the report, added charts, added code to create plots

--- Connect4-Python/create_plots.py
+++ Connect4-Python/create_plots.py
@@ -17,7 +17,10 @@ def plot_data(data, labels, title, x_label, y_label):
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     if len(labels) > 1:
+        # apply legend to the right of the plot
         plt.legend()
+
+
     #and save the plot to a file
     plt.savefig(title + ".png")
     plt.show()
@@ -26,7 +29,7 @@ def plot_data(data, labels, title, x_label, y_label):
 #graph for gammmas
 def graph_for_gammmas():
     vs_offensive = [71, 81, 96, 100]
-    vs_defensive = [96, 99, 100, 100]
+    vs_defensive = [55, 72, 92, 100]
     labels = ["offensive", "defensive"]
     data = [vs_offensive, vs_defensive]
     plot_data(data, labels, "performance against baseline players", "probability to random action",
@@ -34,9 +37,9 @@ def graph_for_gammmas():
 
 def graph_for_depths():
     complex_vs_offensive = [95.1, 95.2, 99.0, 99.9, 99.9, 99.9]
-    complex_vs_defensive = [86.3, 95.5, 93.1, 98.1] # todo need aboud results!
+    complex_vs_defensive = [86.3, 95.5, 93.1, 98.1, 96.4, 98.8]
     ibef2_vs_offensive = [83.1, 76.5, 97.5, 81.8, 98.5, 85.2]
-    ibef2_vs_defensive = [89.9, 85.7, 98.2, 92.3, ] # todo need aboud results!
+    ibef2_vs_defensive = [89.9, 85.7, 98.2, 92.3, 98.7, 94.5]
     labels = ["offensive vs complex", "defensive vs complex", "offensive vs ibef2", "defensive vs ibef2"]
     data = [complex_vs_offensive, complex_vs_defensive, ibef2_vs_offensive, ibef2_vs_defensive]
     plot_data(data, labels, "Compare complex and ibef2 heuristics", "depth of the search tree",
